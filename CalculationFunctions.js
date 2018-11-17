ffda37ab6fcbf9430ff013027a5635a6ee6cfce3
Khesim Reid <khesim@er2-10-245-231-164.wlan.dur.ac.uk>
2018-11-17 15:14:22 +0000

    Started functions for calculations

---
+++ CalculationFunctions.js
@@ -0,0 +1,11 @@
+//Portfolio value function
+function portfolioValue(/*pass dictionary of stock codes and quantities; daily CSV */)
+{
+    for(stock in portfolio)
+    {
+       // value += stock[closing price from close in CSV] * stock[qty];
+    }
+    return value;
+}
+
+function
