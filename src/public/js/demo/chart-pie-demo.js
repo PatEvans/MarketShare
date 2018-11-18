608a0d35b537f87eed6ec3661d1363a924cf2681
Sara Caballero-Bruno <sara2cb@gmail.com>
2018-11-18 06:24:02 +0000

    pie chart

--- src/public/js/demo/chart-pie-demo.js
+++ src/public/js/demo/chart-pie-demo.js
@@ -2,6 +2,11 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
+$.get( "/data/getPortfolioIndustry", function( data ) {
+        $( ".result" ).html( data );
+        alert( "Load was performed." );
+
+
 // Pie Chart Example
 var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
@@ -10,7 +15,8 @@ var myPieChart = new Chart(ctx, {
     labels: ["Utilities", "Consumer Disretionary", "costumer Staples", "Energy", "Healthcare", "Industrial", "Technology",
             "Telecom", "Materials", "Real Estate", "Other"];
     datasets: [{
-      data: [12.21, 15.58, 11.25, 8.32],
+      data: [data.utilities, data.consumerDiscretionary,data.consumerStaples, data.energy, data.healthcare, data.industrial,
+            data.telecom, data.materials, data.realEstate, data.other];
       backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745', '#00ffff', '#ffff00', '#990000',
                         '#40caee', '#0000ff', '#20b2aa', '#ff7f50'],
     }],
@@ -21,4 +27,5 @@ var myPieChart = new Chart(ctx, {
       display: false
     }
   }
+});
 });
