0905e1f46d7b1581ed9f98f4a7de4b383bc78903
Sara Caballero-Bruno <sara2cb@gmail.com>
2018-11-18 07:19:49 +0000

    working pie charts

--- src/public/js/demo/chart-pie-demo.js
+++ src/public/js/demo/chart-pie-demo.js
@@ -2,23 +2,22 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
-$.get( "/data/getPortfolioIndustry", function( data ) {
-        $( ".result" ).html( data );
-        alert( "Load was performed." );
-
 
 // Pie Chart Example
 var ctx = document.getElementById("myPieChart");
+var data = $.get( "/data/getPortfolioIndustry", function(_data) {
+console.log(_data.data.financial);
 var myPieChart = new Chart(ctx, {
   type: 'pie',
   data: {
-    labels: ["Utilities", "Consumer Disretionary", "costumer Staples", "Energy", "Healthcare", "Industrial", "Technology",
+    labels: ["Financial", "Utilities", "Consumer Disretionary", "costumer Staples", "Energy", "Healthcare", "Industrial", "Technology",
             "Telecom", "Materials", "Real Estate", "Other"],
     datasets: [{
-      data: [data.utilities, data.consumerDiscretionary,data.consumerStaples, data.energy, data.healthcare, data.industrial,
-            data.telecom, data.materials, data.realEstate, data.other],
+      data: [_data.data.financial, _data.data.utilities, _data.data.consumerDiscretionary,_data.data.consumerStaples,
+            _data.data.energy, _data.data.healthcare, _data.data.industrial,
+            _data.data.technology, _data.data.telecom, _data.data.materials, _data.data.realEstate, _data.data.other],
       backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745', '#00ffff', '#ffff00', '#990000',
-                        '#40caee', '#0000ff', '#20b2aa', '#ff7f50'],
+                        '#40caee', '#0000ff', '#20b2aa', '#ff7f50', '#000000'],
     }],
   },
   options: {
